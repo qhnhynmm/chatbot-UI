8da1c6ebe9a2142753f40a95c3042390f26a9516
Quang Huy <cbnn7a@gmail.com>
2024-06-02 22:44:02 +0700

    Deploy React app to GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'chatbot-UI',
-  plugins: [react()]
+  base: '/chatbot-UI/', // Đảm bảo đúng tên repository của bạn
+  plugins: [react()],
 });
